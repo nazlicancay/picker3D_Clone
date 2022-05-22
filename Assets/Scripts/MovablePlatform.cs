4c9b05ade83ffe271328007aedb70c2ab86fd298
Nazlıcan Çay <nazlican1306@gmail.com>
2022-05-23 02:03:26 +0300

    MoveablePlatformsUpdate

--- Assets/Scripts/MovablePlatform.cs
+++ Assets/Scripts/MovablePlatform.cs
@@ -2,29 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-
+using TMPro;
 
 public class MovablePlatform : MonoBehaviour
 {
     public int BallNumberToPass;
     public int BallStored = 0 ;
     public GameObject balls;
+    public List<GameObject> Pointballs;
+    public TextMeshPro PointcountText;
 
+
+    private void Update()
+    {
+        PointcountText.text = (Pointballs.Count + " /" + BallNumberToPass);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
+            Collider colider = other.gameObject.GetComponent<Collider>();
+            colider.gameObject.SetActive(false);
            Rigidbody rb =  other.GetComponent<Rigidbody>();
             rb.useGravity = false;
             rb.isKinematic = true;
+            Pointballs.Add(other.gameObject);
             other.transform.parent = balls.transform;
-            if(BallStored >= BallNumberToPass)
+
+            if(Pointballs.Count >= BallNumberToPass)
             {
                 MovePlatformUp();
             }
+           
+           
         }
     }
-
 
 
     public void MovePlatformUp()
@@ -41,9 +53,11 @@ public class MovablePlatform : MonoBehaviour
     public void ContinueGame()
     {
         PlayerMovement.Instance.SpeedUp();
-        for (int i =0; i < transform.childCount - 1; i++)
+        for (int i =0; i < Pointballs.Count - 1; i++)
         {
-            balls.SetActive(false);
+            Pointballs[i].gameObject.SetActive(false);
         }
     }
+
+   
 }
